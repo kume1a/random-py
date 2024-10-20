66155c897efbaa61d8a35cd167c49a4c764f7f2f
kume1a <kumela011@gmail.com>
2024-10-20 13:56:54 +0400

    improve count files in tree

--- Automate/countFilesInTree.py
+++ Automate/countFilesInTree.py
@@ -2,21 +2,28 @@
 
 import os
 
-loc = ""
+def count_files_in_folder(path):
+  folder_count, file_count, line_count = 0, 0, 0
 
-def count_lines(file_path):
-	with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-		return len(f.readlines())
-
-folder_count,file_count,line_count = 0,0,0
-for e in os.walk(loc):
-    current_dir,dirs,files = e
+  for e in os.walk(path):
+    current_dir, dirs, files = e
     folder_count += len(dirs)
     for file in files:
-        if not file.endswith(".g.dart") and not file.endswith('.freezed.dart') and not file.endswith('.config.dart'):
-        	file_count += 1
-        	line_count += count_lines(f'{current_dir}/{file}')
+      if not file.endswith(".g.dart") and not file.endswith('.freezed.dart') and not file.endswith('.config.dart'):
+        file_count += 1
+        line_count += count_lines(f'{current_dir}/{file}')
+  
+  
+  print(f'file count = {file_count}')
+  print(f'folder count = {folder_count}')
+  print(f'line count = {line_count}')
+  print('')
 
-print(f'file count = {file_count}')
-print(f'folder count = {folder_count}')
-print(f'line count = {line_count}')
+def count_lines(file_path):
+  with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+    return len(f.readlines())
+
+count_files_in_folder('C:/Users/Toko/Dev/projects/sonify/sonify/packages/sonify_client/lib')
+count_files_in_folder('C:/Users/Toko/Dev/projects/sonify/sonify/packages/sonify_storage/lib')
+count_files_in_folder('C:/Users/Toko/Dev/projects/sonify/sonify/packages/domain_data/lib')
+count_files_in_folder('C:/Users/Toko/Dev/projects/sonify/sonify/lib')
